2ada2436bb47b600291f281aaddef308541504da
AnuragPandey01 <anuragpandey87187@gmail.com>
2022-09-29 10:16:18 +0530

    save meme to gallery(Home Fragment only)

--- app/src/main/java/com/example/n_meme/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/n_meme/ui/home/HomeFragment.kt
@@ -1,53 +1,74 @@
 package com.example.n_meme.ui.home
 
+import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.Build
 import android.os.Bundle
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.viewpager2.widget.ViewPager2
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import com.example.n_meme.databinding.FragmentHomeBinding
 import com.example.n_meme.model.Meme
 import com.example.n_meme.ui.home.adapter.MemeAdapter
+import kotlinx.coroutines.launch
+import java.io.File
+import java.io.IOException
+import java.io.OutputStream
 
 const val TAG = "HomeFragment"
 class HomeFragment : Fragment() {
 
+    private var currentBitmap: Bitmap? = null
     private var addedToFavourites = mutableListOf<String>()
     private var _binding: FragmentHomeBinding? = null
-    val binding : FragmentHomeBinding
+    val binding: FragmentHomeBinding
         get() = _binding!!
 
-    private val memeAdapter : MemeAdapter by lazy { MemeAdapter() }
+    private val memeAdapter: MemeAdapter by lazy { MemeAdapter() }
     private val memeList: List<Meme> by lazy { memeAdapter.memeList }
-    private val viewModel : HomeViewModel by lazy {
+    private val viewModel: HomeViewModel by lazy {
         ViewModelProvider(this).get(HomeViewModel::class.java)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         //data binding
         _binding = FragmentHomeBinding.inflate(inflater)
         setOnClickListener()
         initViewPager()
         loadMeme()
         try {
-            viewModel.response.observe(viewLifecycleOwner){ response->
-                if(response.isSuccessful){
+            viewModel.response.observe(viewLifecycleOwner) { response ->
+                if (response.isSuccessful) {
                     memeAdapter.setData(response.body()!!.memes)
-                }
-                else{
-                    Toast.makeText(requireContext(),response.code().toString(),Toast.LENGTH_LONG).show()
-                    Log.e(TAG, "onCreateView: ${response.errorBody()} " )
+                } else {
+                    Toast.makeText(requireContext(), response.code().toString(), Toast.LENGTH_LONG)
+                        .show()
+                    Log.e(TAG, "onCreateView: ${response.errorBody()} ")
                 }
             }
-        }
-        catch (e:Exception){
+        } catch (e: Exception) {
             Log.e(TAG, "onCreateView: ${e.stackTrace}")
-            Toast.makeText(requireContext(),"Check your connection",Toast.LENGTH_LONG).show()
+            Toast.makeText(requireContext(), "Check your connection", Toast.LENGTH_LONG).show()
         }
 
         return binding.root
@@ -55,28 +76,44 @@ class HomeFragment : Fragment() {
 
 
     private fun setOnClickListener() {
-
         binding.apply {
             shareMeme.setOnClickListener { shareMeme() }
             favMeme.setOnClickListener { addToFav() }
+            downloadMeme.setOnClickListener {
+                Glide.with(requireContext())
+                    .asBitmap()
+                    .load(memeList[binding.viewpager.currentItem].url)
+                    .into(object : CustomTarget<Bitmap>() {
+                        override fun onResourceReady(
+                            resource: Bitmap,
+                            transition: Transition<in Bitmap>?
+                        ) {
+                            currentBitmap = resource
+                            saveImage(resource)
+                        }
+
+                        override fun onLoadCleared(placeholder: Drawable?) {
+                        }
+                    })
+            }
         }
     }
 
-    private fun loadMeme(){
-        if( memeList.size%7 == 0){
+    private fun loadMeme() {
+        if (memeList.size % 7 == 0) {
             viewModel.getMeme("dankmemes")
         }
     }
 
-    private fun initViewPager(){
+    private fun initViewPager() {
         binding.viewpager.orientation = ViewPager2.ORIENTATION_VERTICAL
         binding.viewpager.adapter = memeAdapter
 
-        binding.viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
+        binding.viewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback() {
             var prevPosition = 0
             override fun onPageSelected(position: Int) {
-                if( prevPosition < position && memeList.size - binding.viewpager.currentItem <= 3){
-                   loadMeme()
+                if (prevPosition < position && memeList.size - binding.viewpager.currentItem <= 3) {
+                    loadMeme()
                 }
                 prevPosition = position
             }
@@ -86,24 +123,106 @@ class HomeFragment : Fragment() {
     private fun shareMeme() {
         val intent = Intent(Intent.ACTION_SEND)
         intent.type = "text/plain"
-        intent.putExtra(Intent.EXTRA_TEXT, "Hey check out this meme ${memeList[binding.viewpager.currentItem].url}")
+        intent.putExtra(
+            Intent.EXTRA_TEXT,
+            "Hey check out this meme ${memeList[binding.viewpager.currentItem].url}"
+        )
         val chooser = Intent.createChooser(intent, "Complete action using...")
         startActivity(chooser)
     }
 
     private fun addToFav() {
-        val toBeAdded = memeList[binding.viewpager.currentItem]
+        val currentMeme = memeList[binding.viewpager.currentItem]
+        if (addedToFavourites.contains(currentMeme.url)) {
+            Toast.makeText(requireContext(), "already added", Toast.LENGTH_SHORT).show()
+            return
+        }
+        viewModel.addToFav(currentMeme.url, currentMeme.title)
 
-        if(addedToFavourites.contains(toBeAdded.url)){
-            Toast.makeText(requireContext(),"already added", Toast.LENGTH_SHORT).show()
+        Toast.makeText(requireContext(), "added to favourite", Toast.LENGTH_SHORT).show()
+        addedToFavourites.add(currentMeme.url)
+    }
+
+    private fun saveImage(bitmap: Bitmap) {
+
+        if (Build.VERSION.SDK_INT < 29 && !hasWriteExternalStoragePerm()) {
+            /*
+            * storing the image bitmap to a global variable so that if perm granted it could save the image by providing bitmap
+            * from the global variable
+            * */
+            currentBitmap = bitmap
+            requestPermission()
             return
         }
 
-        viewModel.addToFav(toBeAdded.url,toBeAdded.title)
+        val currentMeme = memeList[binding.viewpager.currentItem]
+        val name = "${currentMeme.title} by ${currentMeme.author}"
+        val relativePath = Environment.DIRECTORY_PICTURES + File.separator + "Nmeme"
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.ImageColumns.DISPLAY_NAME, name)
+            put(MediaStore.MediaColumns.MIME_TYPE, "image/jpeg")
 
-        Toast.makeText(requireContext(),"added to favourite", Toast.LENGTH_SHORT).show()
-        addedToFavourites.add(toBeAdded.url)
+            // without this part causes "Failed to create new MediaStore record" exception to be invoked (uri is null below)
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+                put(MediaStore.Images.ImageColumns.RELATIVE_PATH, relativePath)
+            }
+        }
+        val contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+
+        var outputStream: OutputStream? = null
+        try {
+
+            /*
+            * creating an outputStream
+            * stream : streams are the sequence of data that are read from the source and written to the destination
+            * An input stream is used to read data from the source. And, an output stream is used to write data to the destination.
+            */
+
+            val resolver = requireContext().contentResolver
+
+            val uri = resolver.insert(contentUri, contentValues)
+                ?: throw IOException("Failed to create new MediaStore record.")
+
+            outputStream = resolver.openOutputStream(uri)
+                ?: throw IOException("Failed to create output stream")
+
+            if (!bitmap.compress(Bitmap.CompressFormat.PNG, 95, outputStream)) {
+                throw IOException("Failed to save bitmap.")
+            }
+            Toast.makeText(requireContext(), "saved image to Gallery", Toast.LENGTH_SHORT).show()
+        } catch (e: IOException) {
+        } finally {
+            outputStream?.close()
+        }
     }
+
+    private fun hasWriteExternalStoragePerm() =
+        requireContext().checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED
+
+    private fun requestPermission() {
+        //showing rationale if denied many time
+        if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(requireContext(), "This permission is needed to store image in external storage", Toast.LENGTH_SHORT).show()
+            return
+        }
+        requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+
+    }
+
+    private val requestPermissionLauncher =
+        registerForActivityResult(
+            ActivityResultContracts.RequestPermission()
+        ) { isGranted: Boolean ->
+            if (isGranted) {
+                saveImage(currentBitmap!!)
+            } else {
+                Toast.makeText(
+                    requireContext(),
+                    "To save image permission is required",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
 
     override fun onDestroyView() {
         Log.d(TAG, "onDestroy: called")
--- app/src/main/java/com/example/n_meme/ui/home/adapter/MemeAdapter.kt
+++ app/src/main/java/com/example/n_meme/ui/home/adapter/MemeAdapter.kt
@@ -1,6 +1,6 @@
 package com.example.n_meme.ui.home.adapter
 
-import android.graphics.drawable.Drawable
+import android.graphics.Bitmap
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -16,7 +16,7 @@ import com.example.n_meme.R
 import com.example.n_meme.model.Meme
 
 
-class MemeAdapter: RecyclerView.Adapter<MemeAdapter.ViewHolder>() {
+class MemeAdapter : RecyclerView.Adapter<MemeAdapter.ViewHolder>() {
 
     private val _memeList: MutableList<Meme> = mutableListOf()
     val memeList: List<Meme>
@@ -24,7 +24,8 @@ class MemeAdapter: RecyclerView.Adapter<MemeAdapter.ViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.meme_item_view,parent,false)
+        val itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.meme_item_view, parent, false)
         return ViewHolder(itemView)
     }
 
@@ -32,36 +33,37 @@ class MemeAdapter: RecyclerView.Adapter<MemeAdapter.ViewHolder>() {
         val context = holder.img.context
         val currMeme = _memeList[position]
 
-         Glide.with(context)
+        Glide.with(context)
+            .asBitmap()
             .load(currMeme.url)
             .diskCacheStrategy(DiskCacheStrategy.RESOURCE)
             .placeholder(R.drawable.placeholder)
             .thumbnail(
                 Glide.with(context)
+                    .asBitmap()
                     .load(currMeme.preview[1])
             )
-             .listener(object : RequestListener<Drawable>{
-                 override fun onLoadFailed(
-                     e: GlideException?,
-                     model: Any?,
-                     target: Target<Drawable>?,
-                     isFirstResource: Boolean
-                 ): Boolean {
-                       holder.img.setImageResource(R.drawable.failed_placeholder)
-                     return false
-                 }
+            .listener(object : RequestListener<Bitmap> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Bitmap>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    holder.img.setImageResource(R.drawable.failed_placeholder)
+                    return false
+                }
 
-                 override fun onResourceReady(
-                     resource: Drawable?,
-                     model: Any?,
-                     target: Target<Drawable>?,
-                     dataSource: DataSource?,
-                     isFirstResource: Boolean
-                 ): Boolean {
-                     return false
-                 }
-
-             })
+                override fun onResourceReady(
+                    resource: Bitmap?,
+                    model: Any?,
+                    target: Target<Bitmap>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    return false
+                }
+            })
             .into(holder.img)
     }
 
@@ -73,12 +75,13 @@ class MemeAdapter: RecyclerView.Adapter<MemeAdapter.ViewHolder>() {
         super.onDetachedFromRecyclerView(recyclerView)
         _memeList.clear()
     }
-    inner class ViewHolder( itemView:View):RecyclerView.ViewHolder(itemView){
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val img: ImageView = itemView.findViewById(R.id.meme_img)
     }
 
-    fun setData(list: List<Meme>){
+    fun setData(list: List<Meme>) {
         _memeList.addAll(list)
-        notifyItemRangeChanged(_memeList.size,7)
+        notifyItemRangeChanged(_memeList.size, 7)
     }
 }
